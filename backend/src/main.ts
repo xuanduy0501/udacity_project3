0dc0f8d4ec60a5f587de308ddf6bcf3080213290
duyblx <xuanduy.wonder123@gmail.com>
2023-06-25 09:40:19 +0700

    build fe + be

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
     .setDescription('Glee2 API')
     .setVersion('1.0')
     .addTag('customTag')
-    .setBasePath(apiVersionPrefix)
+    .setBasePath(apiVersionPrefix)x
     .addBearerAuth() // here is an intentional compile error. Remove the "x" and the backend should compile.
     .build();
   const document = SwaggerModule.createDocument(app, options);
